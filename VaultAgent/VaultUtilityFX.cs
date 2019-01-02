2ca0f65c17dc32bc36e9099de216d02fa10a4cdf
Scott Herrmann Personal <daborg69@gmail.com>
2019-01-02 04:56:10 -0500

    Very minor change to VaultUtilityFX

--- VaultAgent/VaultUtilityFX.cs
+++ VaultAgent/VaultUtilityFX.cs
@@ -5,31 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VaultAgent
-{
-	public static class VaultUtilityFX
-	{
-		public static DateTime ConvertUnixTimeStamp(string unixTimeStamp) {
-			return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(Convert.ToDouble(unixTimeStamp));
-		}
-		public static DateTime ConvertUnixTimeStamp(long unixTimeStamp) {
+namespace VaultAgent {
+	public static class VaultUtilityFX {
+		public static DateTime ConvertUnixTimeStamp (string unixTimeStamp) {
 			return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(Convert.ToDouble(unixTimeStamp));
 		}
 
+
+		public static DateTime ConvertUnixTimeStamp (long unixTimeStamp) {
+			return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(Convert.ToDouble(unixTimeStamp));
+		}
+
+
 		public static string Base64EncodeAscii (string textToEncode) {
-			byte[] rawBytes = ASCIIEncoding.ASCII.GetBytes(textToEncode);
+			byte [] rawBytes = ASCIIEncoding.ASCII.GetBytes(textToEncode);
 			return Convert.ToBase64String(rawBytes);
 		}
 
 
 		public static string Base64DecodeAscii (string textToDecode) {
-			byte[] decoded = Convert.FromBase64String(textToDecode);
+			byte [] decoded = Convert.FromBase64String(textToDecode);
 			return ASCIIEncoding.ASCII.GetString(decoded);
 		}
 
 
 
-
+		// TODO - Not SURE We NEED THIS ANYMORE - Can be replaced with VaultSerializationHelper.FromJSON method.
 		/// <summary>
 		/// This function accepts a JSON string and will convert it into a strongly typed C# class object.  
 		/// Especially useful for converting JSON objects that are sub arrays or sub lists into C# Lists or Dictionaries.
@@ -38,8 +39,6 @@ namespace VaultAgent
 		/// <typeparam name="T">The C# class type to convert the JSON into. </typeparam>
 		/// <param name="json">The JSON string that should be converted.</param>
 		/// <returns>The C# class type to be returned filled with the JSON values.</returns>
-		public static T ConvertJSON<T>(string json) {
-			return JsonConvert.DeserializeObject<T>(json);
-		}
+		public static T ConvertJSON<T> (string json) { return JsonConvert.DeserializeObject<T>(json); }
 	}
 }
